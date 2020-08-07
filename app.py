d965e44cad1f94a5d707067366cc8fc1fa0a8268
Yennie Jun <yennie.jun@gmail.com>
2020-08-07 15:53:06 -0500

    Removing ego network, replacing with edge-filtered network

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import plotly.express as px
 import covid
 from dash.dependencies import Input, Output
+import dash_daq as daq
 
 
 
@@ -18,20 +19,43 @@ server = app.server # the Flask app
 
 app.layout = html.Div(children=[
     html.H1(children='Coronavirus Network Analysis of US News'),
+    # daq.Slider(
+    #     min=0, max=100, value=30, step=1,
+    #     marks={str(val): str(val) for val in range(0,101,10)},
+    # ),
     dcc.Slider(
-        id='slider',
-        min=10,
-        max=100,
-        value=50,
-        marks={str(val): str(val) for val in range(0,101,10)},
-        step=1
+        id='slider_us',
+        min=200,
+        max=400,
+        value=400,
+        marks={str(val): str(val) for val in range(200,401,50)},
+        step=50
     ),
+
     dcc.Graph(
         id='us',
         # figure=covid.network_graph("us")
     ),
 
+    # html.Br(),
+    # html.Br(),
+
     # html.H1(children='Coronavirus Network Analysis of Korean News'),
+
+    # dcc.Slider(
+    #     id='slider_kor',
+    #     min=10,
+    #     max=100,
+    #     value=50,
+    #     marks={str(val): str(val) for val in range(0,101,10)},
+    #     step=1
+    # ),
+
+    # dcc.Graph(
+    #     id='kor',
+    #     # figure=covid.network_graph("us")
+    # ),
+
 
     # dcc.Graph(
     #     id='kor',
@@ -41,9 +65,17 @@ app.layout = html.Div(children=[
 
 @app.callback(
     Output('us', 'figure'),
-    [Input('slider', 'value')])
-def update_graph(slider):
-    return covid.network_graph("us", slider)
+    [Input('slider_us', 'value')])
+def update_graph(slider_us):
+    return covid.network_graph("us", slider_us)
+
+# @app.callback(
+#     Output('kor', 'figure'),
+#     [Input('slider_kor', 'value')])
+# def update_graph2(slider_kor):
+#     return covid.network_graph("us", 10)
+#     # return covid.network_graph("kor", slider_kor)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- covid.py
+++ covid.py
@@ -5,6 +5,9 @@ import plotly.graph_objects as go
 import math
 import ast
 
+def normalize(x, weights):
+    return (x - min(weights)) / (max(weights) - min(weights))
+
 def list_flatten(l, a=None):
     #check a
     if a is None:
@@ -70,7 +73,7 @@ def make_node_trace():
     )
 
 
-def network_graph(country, slider_value):
+def network_graph(country, slider):
     edgelist = pd.read_csv(f"data/{country}_edgelist_weight.csv")
     edgelist.drop(columns="Unnamed: 0", inplace=True)
     nodelist = pd.read_csv(f"data/{country}_nodelist.csv")
@@ -81,8 +84,8 @@ def network_graph(country, slider_value):
     seventyfive = seventyfive.append(edgelist[edgelist['node2'] == "Topic75"])
     seventyfive = seventyfive.sort_values(by="weight", ascending = False)
 
-    edges = seventyfive[:slider_value]
-
+    edges = edgelist[edgelist.weight > slider]
+    graph = nx.Graph()
 
     # get the list of nodes/topics
     topics = list(set(list_flatten(edges[['node1', 'node2']].values.tolist())))
@@ -93,18 +96,33 @@ def network_graph(country, slider_value):
     topic_name_dict = nodelist[nodelist.topic.isin(topics)]
     topic_name_dict["label"] = topic_name_dict.label.apply(lambda a: ast.literal_eval(a))
 
-
-    graph = nx.star_graph(topics)
-    pos_ = nx.spring_layout(graph)
-
     # add edges and weights
     for index, value in edges.iterrows():
         graph.add_edge(value['node1'], value['node2'], weight=value['weight'])
+
+    # https://stackoverflow.com/questions/14283341/how-to-increase-node-spacing-for-networkx-spring-layout
+    nodes_of_largest_component  = max(nx.connected_components(graph), key = len)
+    largest_component = graph.subgraph(nodes_of_largest_component)
+    graph = largest_component
+    dfg = pd.DataFrame(index=graph.nodes(), columns=graph.nodes())
+    for row, data in nx.shortest_path_length(graph):
+        for col, dist in data.items():
+            dfg.loc[row,col] = dist
+
+    dfg = dfg.fillna(dfg.max().max())
+    pos_ = nx.kamada_kawai_layout(graph, dist=dfg.to_dict())
         
     # The annoying workaround to show the labels on the lines
     # https://stackoverflow.com/questions/46037897/line-hover-text-in-plotly
     middle_node_trace = make_middle_node_trace()
 
+    # For each edge, make an edge_trace, append to list
+    # For normalization
+    edge_weights = []
+    for edge in graph.edges():
+        weight = graph.edges()[edge]['weight']
+        edge_weights.append(weight)
+        
     # For each edge, make an edge_trace, append to list
     edge_trace = []
     for edge in graph.edges():
@@ -112,47 +130,60 @@ def network_graph(country, slider_value):
         char_2 = edge[1]
         x0, y0 = pos_[char_1]
         x1, y1 = pos_[char_2]
-
+        
         weight = graph.edges()[edge]['weight']
-
+        
         text = str(weight) + " [" + char_1 + '-' + char_2 + "]"
-        #     print(text)
-
         trace = make_edge([x0, x1, None], [y0, y1, None], text, 
-                          width = weight/400)
-        #                        width = math.log2(weight/1000))
+                          width = ((normalize(weight, edge_weights)+0.1) * 2))
+    #                        width = math.log2(weight/1000))
         edge_trace.append(trace)
-
+        
         middle_node_trace['x'] += tuple([(x0+x1)/2])
         middle_node_trace['y'] += tuple([(y0+y1)/2])
         middle_node_trace['text'] +=  tuple([text])
 
+
     node_trace = make_node_trace()
 
+    # Normalize node weights
+    node_weights = []
+    for node in graph.nodes():
+        x, y = pos_[node]
+        nodesize = nodelist.loc[nodelist['topic'] == node]['totalcount'].values[0]
+        node_weights.append(nodesize)
+        
+        
     # For each node, get the position and size and add to the node_trace
     for node in graph.nodes():
         x, y = pos_[node]
-        nodesize = math.log(nodelist.loc[nodelist['topic'] == node]['totalcount'].values[0]) * 5
+        nodesize = nodelist.loc[nodelist['topic'] == node]['totalcount'].values[0]
+        nodesize = (normalize(nodesize, node_weights)+0.8) * 30
+        
         node_trace['x'] += tuple([x])
         node_trace['y'] += tuple([y])
+        
         nodename = re.search('\d+', node).group()
+        
         node_trace['text'] += tuple(['<b>' + str(nodename) + '</b>'])
         node_trace['marker']['size'] += tuple([nodesize])
 
+    
     node_adjacencies = []
     node_text = []
     node_annotations = []
 
     for node,adjacencies in enumerate(graph.adjacency()):
-        nodesize = nodelist.loc[nodelist['topic'] == "Topic"+str(node)]['totalcount'].values[0]
+        nodesize = nodelist.loc[nodelist['topic'] == adjacencies[0]]['totalcount'].values[0]
+        # nodesize = (normalize(nodesize, node_weights)+1) * 30
+
         node_adjacencies.append(nodesize)
 
         topic_info = topic_name_dict[topic_name_dict['topic'] == adjacencies[0]]
         topic_label = topic_info.label.values[0][0:8] # Just the first 4
         topic_label = ' '.join(["-".join(topic_label[i:i+2]) for i in range(0, len(topic_label), 2)])
 
-
-        node_text.append(topic_label)
+        node_text.append(topic_label + f" [{nodesize}]")
         nodename = re.search('\d+', adjacencies[0]).group()
 
         node_annotations.append(
@@ -161,8 +192,19 @@ def network_graph(country, slider_value):
                 y = pos_[adjacencies[0]][1],
                 text = nodename, # node name that will be displayed
                 font = dict(color='white', size=10),
-                showarrow=False, arrowhead=1, ax=-10, ay=-10)
-            )
+                showarrow=False, arrowhead=1, ax=-10, ay=-10))
+
+
+    # Specifically for Topic 75
+    seventyfive2 = edges[edges['node1'] == "Topic75"]
+    seventyfive2 = seventyfive2.append(edges[edges['node2'] == "Topic75"])
+    seventyfive2 = seventyfive2.sort_values(by="weight", ascending = False)
+
+    subtitle = ["Topic 75 [contact tracing] edges:"]
+    for i, row in seventyfive2.iterrows():
+        subtitle.append(f"{row.node1}-{row.node2}[{row.weight}]")
+
+
 
     # Plot the graph
     node_trace.marker.color = node_adjacencies
@@ -172,6 +214,7 @@ def network_graph(country, slider_value):
                  layout=go.Layout(
                     # title='Network Analysis of Coronavirus Topics',
                     titlefont_size=16,
+                    xaxis_title='<br>'.join([s for s in subtitle]),
                     showlegend=False,
                     hovermode='closest',
                     margin=dict(b=20,l=5,r=5,t=40),
